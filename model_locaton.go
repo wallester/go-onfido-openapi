f146bc644b8b410d323fb8ded2a7bc20ad45c183
tanyapuchkova <tatjana.puchkova@wallester.com>
2022-09-01 11:02:27 +0300

    formatting

--- model_locaton.go
+++ model_locaton.go
@@ -2,8 +2,8 @@ package onfido_openapi
 
 // Location struct for Location
 type Location struct {
-// The IP address of the applicant
-IPAddress *string `json:"ip_address,omitempty"`
-// The 3 character ISO country code of the country where the applicant resides
-CountryOfResidence *string `json:"country_of_residence,omitempty"`
+	// The IP address of the applicant
+	IPAddress *string `json:"ip_address,omitempty"`
+	// The 3 character ISO country code of the country where the applicant resides
+	CountryOfResidence *string `json:"country_of_residence,omitempty"`
 }
